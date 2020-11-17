ecef14e1c3eea1b752548cd5a72dc997381393d7
Georgy Parhomenko <g.parhomenko@itransition.com>
2020-11-17 21:35:08 +0300

    Remove useless docs

--- aircompany-master/Java/src/main/java/Airport.java
+++ aircompany-master/Java/src/main/java/Airport.java
@@ -8,10 +8,6 @@ import java.util.List;
 import java.util.Comparator;
 import java.util.stream.Collectors;
 
-// version: 1.1
-// made by Vitali Shulha
-// 4-Jan-2019
-
 public class Airport {
     private List<? extends Plane> planes;
 
