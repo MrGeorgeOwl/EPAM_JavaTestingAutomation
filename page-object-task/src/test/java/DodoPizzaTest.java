0960ddfd22dc35e49d109510971ac6ca190a42c4
Georgy Parhomenko <g.parhomenko@itransition.com>
2020-12-14 00:32:31 +0300

    Refactor test

--- page-object-task/src/test/java/DodoPizzaTest.java
+++ page-object-task/src/test/java/DodoPizzaTest.java
@@ -4,38 +4,35 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import page.CartPage;
-import page.MainPage;
-
+import page.MenuPage;
 
 public class DodoPizzaTest {
 
     WebDriver driver;
-    MainPage mainPage;
+    MenuPage mainPage;
     CartPage cartPage;
 
-    @BeforeClass
+    @BeforeMethod
     private void setUp() {
-        System.setProperty("webdriver.chrome.driver", "/drivers/chromedriver");
+        driver = setUpDriver();
+
+        mainPage = new MenuPage(driver);
+        cartPage = new CartPage(driver);
+    }
+
+    private WebDriver setUpDriver() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("disable-gpu");
         options.addArguments("window-size=1280,720");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        driver = new ChromeDriver(options);
-
-        mainPage = new MainPage(driver);
-        cartPage = new CartPage(driver);
-    }
-
-    @AfterClass
-    private void teardown() {
-        driver.quit();
+        return new ChromeDriver(options);
     }
 
     @AfterMethod
-    private void clearCache() {
-        driver.manage().deleteAllCookies();
+    private void teardown() {
+        driver.quit();
     }
 
     @Test
